ab7ae728a1c5cfb3fc34e9ce63ff308b4f4feade
Andreza Silveira Lopes <181710121@senacinformatica.br>
2018-12-05 09:54:25 -0200

    comit pagina index

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Clinica M171</title> 
     </head>
     <body>
+
         <?php
-        // put your code here
+        //chama o arquivo verificando se já não foi chamado antes
+        require_once 'menu.php';
+        ?>
+
+
+
+        <br><br><br><br>
+        <h1 align="clinica"> <i>  Bem-vindo a Clinica </i> </h1>
+
+
+
+        <?php
         ?>
     </body>
 </html>
