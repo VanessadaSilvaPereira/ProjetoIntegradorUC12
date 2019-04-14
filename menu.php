b0a4617511f02f3f55eef10ea46b971b3e72c835
Vanessa da Silva Pereira <vanessa.pereira787@gmail.com>
2019-04-14 18:49:08 -0300

    ajustes no menu, e no index.2

--- menu.php
+++ menu.php
@@ -1,11 +1,15 @@
 
 <header> 
     
-    <div style="background-image: url(imagens/inicial.jpg); height:500px;background-position:center">
+    <div style="background-image: url(imagens/olhos.png); height:400px;
+         background-position:center;
+         margin-top:0;
+         border: 2px solid transparent;
+         background-repeat: no-repeat;">
     
-   <div id="botoesMenu"><a class="botoesMenu" href="index.php"><button>Início</button></a>
+        <div id="botoesMenu"><a class="botoesMenu" href="index.php"><button>Início</button></a>
     
-    <a href="especialidades.php" class="botoesMenu"><button>Especialidades</button></a>
+       <a href="especialidades.php" class="botoesMenu"><button>Especialidades</button></a>
     
     <a href="medicos.php" class="botoesMenu"><button>Médicos</button></a>
 
@@ -13,7 +17,7 @@
    
     <a href="fiqueDeOlho.php" class="botoesMenu"><button>Fique de Olho</button></a></div>
     
-    <div style="color: white; font-size: 50px;text-align: center"><i>Bem-vindo a JG Clínica Oftalmo</i></div>
+    <div style="color: black; font-size: 50px;text-align:center; margin-top:18%;"><i>Bem-vindo a JG Clínica Oftalmo</i></div>
     
     </div>
     
