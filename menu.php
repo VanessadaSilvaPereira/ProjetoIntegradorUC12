aa015938d90f687e758766a0f7019f222756cf28
Vanessa da Silva Pereira <vanessa.pereira787@gmail.com>
2019-04-16 17:19:56 -0300

    refatoração do código para melhor organização

--- menu.php
+++ menu.php
@@ -1,24 +1,31 @@
 
 <header> 
 
-   
-        <div class="botoesMenu" style="float:right"><a href="fiqueDeOlho.php"><button>Fique de Olho</button></a></div>
-        <div style="float: right" class="botoesMenu"><a href="exames.php"><button>Exames</button></a></div>
-        <div style="float: right" class="botoesMenu"><a href="medicos.php"><button>Médicos</button></a></div>
-        <div style="float: right" class="botoesMenu"><a  href="especialidades.php"><button>Especialidades</button></a></div>
-        <div ><a class="botoesMenu" href="index.php" style="float: right"<div><img src="imagens/casa.png"></a></div>
+
+    <div class="botoesMenu"><a href="fiqueDeOlho.php"><button>Fique de Olho</button></a></div>
+    <div class="botoesMenu"><a href="exames.php"><button>Exames</button></a></div>
+    <div class="botoesMenu"><a href="medicos.php"><button>Médicos</button></a></div>
+    <div class="botoesMenu"><a  href="especialidades.php"><button>Especialidades</button></a></div>
+    <div ><a class="botoesMenu" href="index.php"<div><img src="imagens/casa.png"></a></div>
 
 
-        <div class="limparFloat"></div>
+            <div class="limparFloat"></div>
 
-        <div style="background-image: url(imagens/olhos.png); height:270px;
-             background-position:center;
-             background-repeat: no-repeat;">
-
-
-          
+            <div id="divOlhos"></div>
             
+            <div id="BemVindo"><i>Bem-vindo a JG Clínica Oftalmo</i></div>
 
-        </div>
-<div style="color: black; font-size: 50px;text-align:center; margin-top:0%;"><i>Bem-vindo a JG Clínica Oftalmo</i></div>
-</header>
+            <style>
+                #BemVindo{
+                    color:black;
+                    font-size:50px;
+                    text-align:center;
+                    margin-top:0%;
+                }
+                #divOlhos{
+                    background-image: url(imagens/olhos.png); height:270px;
+                    background-position:center;
+                    background-repeat: no-repeat;
+                }
+            </style>
+            </header>
