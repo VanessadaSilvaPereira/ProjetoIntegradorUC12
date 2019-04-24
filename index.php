2ef262e196ed60f1339e5428a1e48025e8c11732
Vanessa da Silva Pereira <vanessa.pereira787@gmail.com>
2019-04-24 11:41:53 -0300

    mudanças nas disposições das divs dos medicos.2

--- index.php
+++ index.php
@@ -11,44 +11,44 @@
         <?php
         require_once 'menu.php';
         ?>
-        
+
         <div id="BemVindo">Bem-vindo a JG Clínica Oftalmo</div>
 
         <div id="historiaClinica"><div id="TituloHistoriaClinica"><h1><i>Sobre a Clínica...</i></h1></div>
 
-                <p>A JG é uma Clínica de Oftalmologia com mais de 25 anos de experiência.
+            <p>A JG é uma Clínica de Oftalmologia com mais de 25 anos de experiência.
                 Localizada na cidade de Três Lagoas.</p>
-                <p>Foi Fundada pelos DRs. Jorge e Geraldo.
+            <p>Foi Fundada pelos DRs. Jorge e Geraldo.
                 Hoje seguida por seus filhos na mesma profissão.</p>
-                <p>No início de suas carreiras, seus procedimentos se dividiam em atendimentos na
-                    clínica e em salas alugadas no Hospital da cidade.</p>
-                <p>Através da nova visão dos filhos, empreenderam em Clínica própria para procedimentos
-                    mais amplos e complexos.</p>
+            <p>No início de suas carreiras, seus procedimentos se dividiam em atendimentos na
+                clínica e em salas alugadas no Hospital da cidade.</p>
+            <p>Através da nova visão dos filhos, empreenderam em Clínica própria para procedimentos
+                mais amplos e complexos.</p>
 
-            </div>
-            <div id="comoVemos"><h2>Como é que nós vemos ?</h2>
-                <ul><li>A luz reflete-se nos objetos e segue em linha reta para o olho.</li>
-                    <li>A luz passa através da córnea, para dentro da pupila e através do cristalino.</li>
-                    <li>A córnea e o cristalino ajustam-se à luz (refratam) para que a retina foque.</li>
-                    <li>Os fotorrecetores na retina convertem a luz em impulsos elétricos.</li>
-                    <li>Os impulsos elétricos atravessam o nervo ótico até ao cérebro.</li>
-                    <li>O cérebro processa os sinais para criar uma imagem.</li>
-                </ul>
-            </div>
-        
-            <div id="voceSabia">
-                <h2>Você Sabia?</h2>
-                <h3>Crianças devem fazer exame oftalmológico de forma regular desde o nascimento.</h3>
-                <p>As crianças pequenas apresentam uma visão que não é definitiva 
-                    e precisam ser acompanhadas por oftalmologista 
-                    para que alterações sejam percebidas ainda em tempo hábil para tratamento.</p>
-            </div>
-        
-      
-<div class="limparFloat"></div>
-<?php
-    require_once './frmFaleConosco.php';
-    ?>
+        </div>
+        <div id="comoVemos"><h2>Como é que nós vemos ?</h2>
+            <ul><li>A luz reflete-se nos objetos e segue em linha reta para o olho.</li>
+                <li>A luz passa através da córnea, para dentro da pupila e através do cristalino.</li>
+                <li>A córnea e o cristalino ajustam-se à luz (refratam) para que a retina foque.</li>
+                <li>Os fotorrecetores na retina convertem a luz em impulsos elétricos.</li>
+                <li>Os impulsos elétricos atravessam o nervo ótico até ao cérebro.</li>
+                <li>O cérebro processa os sinais para criar uma imagem.</li>
+            </ul>
+        </div>
+
+        <div id="voceSabia">
+            <h2>Você Sabia?</h2>
+            <h3>Crianças devem fazer exame oftalmológico de forma regular desde o nascimento.</h3>
+            <p>As crianças pequenas apresentam uma visão que não é definitiva 
+                e precisam ser acompanhadas por oftalmologista 
+                para que alterações sejam percebidas ainda em tempo hábil para tratamento.</p>
+        </div>
+
+
+        <div class="limparFloat"></div>
+        <?php
+        require_once './frmFaleConosco.php';
+        ?>
     </body>
-    
+
 </html>
